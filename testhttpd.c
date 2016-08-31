28a7eaea8d7fd3b5d30d5d0f05adcdac86aa8763
Dan Stutzman <dan@danstutzman.com>
2016-08-30 19:54:40 -0600

    Refactor testhttpd.c to make it easier to read.  Also return 405 METHOD_NOT_ALLOWED if not a GET request.

--- testhttpd.c
+++ testhttpd.c
@@ -3,63 +3,54 @@
 #include <string.h>
 #include <stdio.h>
 
-#define PAGE "<html><head><title>libmicrohttpd demo</title>"\
-             "</head><body>libmicrohttpd demo</body></html>"
+static int access_handler_callback(void* _contextInput,
+    struct MHD_Connection* connection, const char* url, const char* method,
+    const char* version, const char* upload_data, size_t* upload_data_size,
+    void** contextOutput) {
 
-static int ahc_echo(void * cls,
-        struct MHD_Connection * connection,
-        const char * url,
-        const char * method,
-                    const char * version,
-        const char * upload_data,
-        size_t * upload_data_size,
-                    void ** ptr) {
-  static int dummy;
-  const char * page = cls;
-  struct MHD_Response * response;
-  int ret;
+  // if first time, just return MHD_YES
+  if (*contextOutput == NULL) {
+    static int old_connection_marker;
+    *contextOutput = &old_connection_marker;
+    return MHD_YES;
+  } else {
+    *contextOutput = NULL;
+  }
 
-  if (0 != strcmp(method, "GET"))
-    return MHD_NO; /* unexpected method */
-  if (&dummy != *ptr)
-    {
-      /* The first time only the headers are valid,
-         do not respond in the first round... */
-      *ptr = &dummy;
-      return MHD_YES;
-    }
-  if (0 != *upload_data_size)
-    return MHD_NO; /* upload data in a GET!? */
-  *ptr = NULL; /* clear context pointer */
-  response = MHD_create_response_from_buffer (strlen(page),
-                                              (void*) page,
-                  MHD_RESPMEM_PERSISTENT);
-  ret = MHD_queue_response(connection,
-         MHD_HTTP_OK,
-         response);
+  const char* html;
+  unsigned int status_code;
+  if (strcmp(method, "GET") != 0) {
+    html = "Method not allowed";
+    status_code = MHD_HTTP_METHOD_NOT_ALLOWED;
+  } else if (*upload_data_size != 0) {
+    html = "Bad request: can't upload data in a GET";
+    status_code = MHD_HTTP_BAD_REQUEST;
+  } else {
+    html = "Success";
+    status_code = MHD_HTTP_OK;
+  }
+
+  struct MHD_Response* response = MHD_create_response_from_buffer (strlen(html),
+    (void*) html, MHD_RESPMEM_PERSISTENT);
+  int did_queue_succeed = MHD_queue_response(connection, MHD_HTTP_OK, response);
   MHD_destroy_response(response);
-  return ret;
+  return did_queue_succeed;
 }
 
-int main(int argc,
-   char ** argv) {
-  struct MHD_Daemon * d;
+int main(int argc, char ** argv) {
   if (argc != 2) {
-    printf("%s PORT\n",
-     argv[0]);
+    printf("%s PORT\n", argv[0]);
     return 1;
   }
-  d = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION,
-           atoi(argv[1]),
-           NULL,
-           NULL,
-           &ahc_echo,
-           PAGE,
-           MHD_OPTION_END);
-  if (d == NULL)
+  int port = atoi(argv[1]);
+
+  fprintf(stderr, "Running web server on port %d...\n", port);
+  struct MHD_Daemon* d = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION,
+    port, NULL, NULL, &access_handler_callback, NULL, MHD_OPTION_END);
+  if (d == NULL) {
+    fprintf(stderr, "Fatal error: MHD_start_daemon returned NULL\n");
     return 1;
-  (void) getc (stdin);
-  MHD_stop_daemon(d);
+  }
+  pause(); /* never return; run web server until Ctrl-C pressed */
   return 0;
 }
-
